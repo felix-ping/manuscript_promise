a37c122380f46be16e63b5743c90e96e838c8652
felix-ping <felix-ping@qq.com>
2020-05-31 23:43:49 +0800

    debugger

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,42 @@
 import * as chai from 'chai'
+import Promise from '../promise'
 
 const assert = chai.assert
 
-describe('test',()=>{
-  it('equal',()=>{
+describe('Promise',()=>{
+  it('promise 是一个类',()=>{
+    console.log(`Promises`, Promise)
+    assert.isFunction(Promise)
+    assert.exists(Promise.prototype)
+  }),
+  it('promise 必须接收一个函数',()=>{
+    assert.throw(()=>{
+      //@ts-ignore
+      new Promise(1)
+    })
+  }), 
+  it('promise 必须有一个then方法',()=>{
+    const promise = new Promise(()=>{})
+    assert.isFunction(promise.then)
+  }),
+  it('promise 中传入的fn立即执行',()=>{
+    let boolean = false
+    new Promise(()=>{
+      boolean= true
+    })
     //@ts-ignore
-    assert(1 === 2)
+    assert(boolean === true)
+  }),
+  it('promise 传入两个方法',()=>{
+    const promise = new Promise((resolve,reject)=>{
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
+    })
+  }),
+  it('promise 传入两个方法',()=>{
+    const promise = new Promise((resolve,reject)=>{
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
+    })
   })
-})
+})  
